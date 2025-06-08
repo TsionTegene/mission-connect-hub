313eac98d8ad806f785eb81607da1eea6b66bb7f
TsionTegene <tsionawitua@gmail.com>
2025-06-08 10:13:06 -0700

    last commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // src/services/api.ts
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
